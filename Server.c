5d50fb0be6ee35af946247cbd4af48d4cc5e5361
Jesús López <jesuslopez@wifi-69-32.uc3m.es>
2017-11-06 17:29:11 +0100

    final

--- Server.c
+++ Server.c
@@ -40,6 +40,7 @@ int main(int argc, char *argv[])
 	case 3:
 		m = argv[1];
 		mode = argv[2];
+		printf("TIME server running in port 6898\n");
 		break;
 	default:
 		fprintf(stderr, "usage: %s <-m> <s>\n", argv[0]);
@@ -107,46 +108,22 @@ int main(int argc, char *argv[])
 		//       hostp->h_name, hostaddrp);
 		printf("Received request from Client: %s:%d\n", hostaddrp, PORT);
 
-		int seconds;
+		int seconds, bytes_sent;
 
 		if (!fork()) { // this is the child process
 			close(sockfd); // child doesn't need the listener
 			while(1){
 				seconds = time(NULL) + 2208988800;
 				seconds = htonl(seconds);
-				int bytes_sent = send(new_fd,&seconds,sizeof(int),0);
+				bytes_sent = send(new_fd, &seconds, sizeof(int), 0);
 				sleep(1);
 			}
 			close(new_fd);
 			exit(0);
 		}
 		close(new_fd);
-
 	}
-
-		/*if(fork() == 0){	// This is a child Process
-			uint32_t buffer;
-			time_t nowtime;
-			int bytes_sent;
-			do{
-				nowtime = time(NULL);
-				buffer = htonl(nowtime + 2208988800);
-				bytes_sent = send(new_fd,&buffer,sizeof(uint32_t),0);
-				sleep(1);
-					if(0) {
-						char time_str[64];
-						strftime(time_str,64,"%c", localtime(&nowtime));
-						//printf("Enviado %s (%d bytes) a %s\n", time_str, bytes_sent, inet_ntoa((struct in_addr)client_socket.sin_addr));
-					}
-
-				}while(bytes_sent > 0);
-				/*printf("Conexión terminada con %s\n",inet_ntoa((struct in_addr)client_socket.sin_addr));
-				Esto no lo imprime nunca,
-				usé valgrind y descubrí que cerraba el hijo con un SIGPIPE (13),
-				así que añadí un handler para esa señal.
-				*/
-			//}
-		close(sockfd);  // parent doesn't need this
+	close(sockfd);  // parent doesn't need this
 	return 0;
 }
 
