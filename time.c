93a3494c6f3d16b7cdf57b2a4f52f1a139e77d05
Jesús López <jesuslopez@MacBook-Pro-de-Jesus.local>
2017-11-06 21:39:46 +0100

    final

--- time.c
+++ time.c
@@ -11,15 +11,13 @@
 #include <time.h>
 #include <arpa/inet.h>
 
-#define DEFAULT_PORT 37
-#define BACKLOG 10	 // how many pending connections queue will hold
-#define SERVER_PORT 6898
+#define DEFAULT_PORT 37 // Puerto por defecto
+#define BACKLOG 10	 // conexiones que soporta en la cola el servidor
+#define SERVER_PORT 6898 // 6000 + 3 ultimos digitos (898)
 
-int sockfd;
-
-void sigint_handler(int signo)
+void sigint_handler(int signo) // Ctrl-C
 {
-	/* things to do after a Ctrl-C, before finishing the program*/
+	/* Después de Ctrl-C, antes de acabar el programa*/
 	puts("SIGINT received, closing program");
 	exit(0);
 }
@@ -28,22 +26,21 @@ void sigint_handler(int signo)
 int main(int argc, char *argv[])
 {
 	struct sockaddr_in serveraddr;
-  struct sockaddr_in clientaddr; // client addr
+  struct sockaddr_in clientaddr;
   struct hostent *server;
-  struct hostent *hostp; // client host info
+  struct hostent *hostp; // info cliente host
 	char *h;
   char *host;
   char *p;
   char *m;
   char *mode;
-  char *hostaddrp; // dotted decimal host addr string
+  char *hostaddrp; // addr string decimal host
 	int i, debug, port, seconds, n, sockfd, new_fd, optval;
-  uint clientlen; // byte size of client's address
+  uint clientlen; // longitud direccion cliente
 
-  /* handler SIGINT signal*/
-	signal(SIGINT, sigint_handler);
+	signal(SIGINT, sigint_handler); // Señal de cerrar programa
 
-	for(i=0; i<argc; i++){
+	for(i=0; i<argc; i++){ //Comprobación de argumentos
 		if(strcmp(argv[i],"-h") == 0){
 			host = argv[i+1];
 		}
@@ -58,29 +55,30 @@ int main(int argc, char *argv[])
 		}
 	}
 
-  if(strcmp (mode,"ct") == 0){
-      printf("TCP_Client\n");
-      /* socket: create the socket */
+
+
+/**/if(strcmp (mode,"ct") == 0){ // Cliente TCP
+      /* socket: crear el socket */
       sockfd = socket(AF_INET, SOCK_STREAM, 0);
       if (sockfd < 0) {
               perror("ERROR opening socket");
               exit(0);
       }
 
-      /* gethostbyname: get the server's DNS entry */
+      /* gethostbyname: DNS del servidor */
       server = gethostbyname(host);
       if (server == NULL) {
               fprintf(stderr,"ERROR, no such host: %s\n", host);
               exit(0);
       }
 
-      /* build the server's Internet address */
+      /* Construir direcciones del servidor internet */
       bzero((char *) &serveraddr, sizeof(serveraddr));
       serveraddr.sin_family = AF_INET;
       bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
       serveraddr.sin_port = htons(port);
 
-      /* connect: create a connection with the server */
+      /* connect: crear una conexion con el servidor */
       if (connect(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
               perror("ERROR connecting");
               exit(0);
@@ -90,6 +88,7 @@ int main(int argc, char *argv[])
 
 			while(1){
 
+				// recibir bytes del servidor
       	n = recv(sockfd, &seconds, sizeof(int), 0);
       	if (n < 0) {
               perror("ERROR reading from socket");
@@ -98,150 +97,149 @@ int main(int argc, char *argv[])
 				if(n==0){
 					exit(0);
 				}
-
+				// Conversion de fecha y hora
       	time_t sec_today = ntohl(seconds) - 2208988800;
 
       	strftime(output, 4096, "%c", localtime(&sec_today));
-
+				// Sacar fecha y hora por pantalla
       	printf("%s\n", output);
 			}
-
+			// Cierre del socket (cierre de conexion)
       close(sockfd);
 
     }
 
-		if(strcmp (mode,"s") == 0){
-      //Server();
-			printf("TIME server running in port 6898\n");
-			/* socket: create the parent socket */
-			sockfd = socket(AF_INET, SOCK_STREAM, 0);
-			if (sockfd < 0) {
-							perror("ERROR opening socket");
-							exit(0);
-			}
 
-			/* setsockopt: lets s rerun the server immediately after we kill it.
- 			* Eliminates "ERROR on binding: Address already in * use"
- 			* error. */
-			optval = 1;
-			setsockopt(new_fd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
 
-			/* build the server's Internet address */
-			bzero((char *) &serveraddr, sizeof(serveraddr));
-			serveraddr.sin_family = AF_INET;
-			serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-			serveraddr.sin_port = htons(SERVER_PORT);
-
-			/* bind: associate the parent socket with a port */
-			if (bind(sockfd, (struct sockaddr *) &serveraddr,
-				 sizeof(serveraddr)) < 0) {
-							perror("ERROR on binding");
-							exit(0);
-			}
-
-			/* listen: make this socket ready to accept connection requests */
-			if (listen(sockfd, BACKLOG) < 0) {
-							perror("ERROR on listen");
-							exit(0);
-			}
-
-			/* wait for a connection request */
-			while(1) {  // main accept() loop
-				clientlen = sizeof(clientaddr);
-				new_fd = accept(sockfd, (struct sockaddr *)&clientaddr,&clientlen);
-
-				if (new_fd == -1) {
-					perror("ERROR on accept");
-					exit(0);
-				}
-
-				/* gethostbyaddr: determine who sent the message */
-				hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
-							  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-				if (hostp == NULL) {
-								perror("ERROR on gethostbyaddr");
-								exit(0);
-				}
-				hostaddrp = inet_ntoa(clientaddr.sin_addr);
-				if (hostaddrp == NULL) {
-								perror("ERROR on inet_ntoa\n");
-								exit(0);
-				}
-
-				printf("Received request from Client: %s : %d\n", hostaddrp, SERVER_PORT);
-
-				int seconds, bytes_sent;
-
-				if (!fork()) { // this is the child process
-					close(sockfd); // child doesn't need the listener
-					while(1){
-						seconds = time(NULL) + 2208988800;
-						seconds = htonl(seconds);
-						bytes_sent = send(new_fd, &seconds, sizeof(int), 0);
-						if (bytes_sent == -1) {
-							perror("ERROR on send");
-							exit(0);
-						}
-						sleep(1);
-					}
-					close(new_fd);
-					exit(0);
-				}
-				close(new_fd);
-			}
-			close(sockfd);  // parent doesn't need this
-    }
-
-		if(strcmp (mode,"cu") == 0){
-      //UDP_Client();
-      printf("UDP_Client\n");
-			/* socket: create the socket */
+/**/if(strcmp (mode,"cu") == 0){ // Cliente UDP
+			/* socket: crear el socket */
 			sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 			if (sockfd < 0) {
 							perror("ERROR opening socket");
 							exit(0);
 			}
 
-			/* gethostbyname: get the server's DNS entry */
+			/* gethostbyname: DNS del servidor */
 			server = gethostbyname(host);
 			if (server == NULL) {
 							fprintf(stderr,"ERROR, no such host: %s\n", host);
 							exit(0);
 			}
 
-			/* build the server's Internet address */
+			/* Construir direcciones del servidor internet */
 			bzero((char *) &serveraddr, sizeof(serveraddr));
 			serveraddr.sin_family = AF_INET;
 			bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
 			serveraddr.sin_port = htons(DEFAULT_PORT);
 
-			/* connect: create a connection with the server */
+			/* connect: crear una conexion con el servidor */
       if (connect(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
               perror("ERROR connecting");
               exit(0);
       }
 
-				//UDP mandar datagrama vacio
+			// Cliente UDP manda datagrama vacio
 			n = send(sockfd, NULL, 0, 0);
 			if (n < 0) {
 							perror("ERROR writing to socket");
 							exit(0);
 			}
 			char output[4096];
-
+			// Recibir bytes del servidor
 			n = recv(sockfd, &seconds, sizeof(int), 0);
 			if (n < 0) {
 							perror("ERROR reading from socket");
 							exit(0);
 			}
-
+			// Conversion fecha y hora
 			time_t sec_today = ntohl(seconds) - 2208988800;
 
 			strftime(output, 4096, "%c", localtime(&sec_today));
-
+			// Sacar por pantalla fecha y hora
 			printf("%s\n", output);
 
     }
+
+		
+
+		/**/if(strcmp (mode,"s") == 0){ // Servidor
+
+					printf("TIME server running in port 6898\n");
+					/* socket: crear el socket padre */
+					sockfd = socket(AF_INET, SOCK_STREAM, 0);
+					if (sockfd < 0) {
+									perror("ERROR opening socket");
+									exit(0);
+					}
+
+					/* setsockopt: Reactivar el servidor cuando se cierra. */
+					optval = 1;
+					setsockopt(new_fd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
+
+					/* Construir direcciones del servidor internet */
+					bzero((char *) &serveraddr, sizeof(serveraddr));
+					serveraddr.sin_family = AF_INET;
+					serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+					serveraddr.sin_port = htons(SERVER_PORT);
+
+					/* bind: asociar un puerto al socket padre */
+					if (bind(sockfd, (struct sockaddr *) &serveraddr,
+						 sizeof(serveraddr)) < 0) {
+									perror("ERROR on binding");
+									exit(0);
+					}
+
+					/* listen: Hacer que el socket escuche peticiones */
+					if (listen(sockfd, BACKLOG) < 0) {
+									perror("ERROR on listen");
+									exit(0);
+					}
+
+					/* Esperar a la conexion */
+					while(1) {  // accept() loop principal
+						clientlen = sizeof(clientaddr);
+						new_fd = accept(sockfd, (struct sockaddr *)&clientaddr,&clientlen);
+
+						if (new_fd == -1) {
+							perror("ERROR on accept");
+							exit(0);
+						}
+
+						/* gethostbyaddr: Determinar quién envió el mensaje */
+						hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+									  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+						if (hostp == NULL) {
+										perror("ERROR on gethostbyaddr");
+										exit(0);
+						}
+						hostaddrp = inet_ntoa(clientaddr.sin_addr);
+						if (hostaddrp == NULL) {
+										perror("ERROR on inet_ntoa\n");
+										exit(0);
+						}
+						// Dirección IP del cliente y puerto asignado
+						printf("Received request from Client: %s : %d\n", hostaddrp, SERVER_PORT);
+
+						if (!fork()) { // Proceso hijo
+							close(sockfd); // El hijo no necesita escuchar
+							while(1){
+								seconds = time(NULL) + 2208988800;
+								seconds = htonl(seconds);
+								n = send(new_fd, &seconds, sizeof(int), 0);
+								if (n == -1) {
+									perror("ERROR on send");
+									exit(0);
+								}
+								// Dormir un segundo
+								sleep(1);
+							}
+							close(new_fd);
+							exit(0);
+						}
+						close(new_fd);
+					}
+					close(sockfd);  // El padre no necesita esto
+		    }
 
 	return(0);
 }
