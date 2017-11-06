6d688347f8c40e13003131d7ec812bc66b54853d
jesuslopezbr <31916753+jesuslopezbr@users.noreply.github.com>
2017-11-06 12:12:35 +0100

    Add files via upload

---
+++ Server.c
@@ -0,0 +1,131 @@
+/*
+ * EchoServer_conc.c
+ * Aplicaciones Telematicas - Grado en Ingenieria Telematica
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
+
+#define BACKLOG 10	 // how many pending connections queue will hold
+#define BUFSIZE 1024
+#define PORT 6898
+
+void sigchld_handler(int s);
+
+int main(int argc, char *argv[])
+{
+	time_t timer;
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+  uint clientlen; // byte size of client's address
+  struct sockaddr_in serveraddr; // server's addr
+  struct sockaddr_in clientaddr; // client addr
+  struct hostent *hostp; // client host info
+  char *hostaddrp; // dotted decimal host addr string
+  int optval; // flag value for setsockopt
+	char *mode;
+	char *m;
+
+	switch (argc) {
+	case 3:
+		m = argv[1];
+		mode = argv[2];
+		break;
+	default:
+		fprintf(stderr, "usage: %s <-m> <s>\n", argv[0]);
+    exit(1);
+  }
+
+        /* socket: create the parent socket */
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) {
+                perror("ERROR opening socket");
+								exit(0);
+				}
+
+        /* setsockopt: lets s rerun the server immediately after we kill it.
+	 * Eliminates "ERROR on binding: Address already in * use"
+	 * error. */
+        optval = 1;
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval , sizeof(int));
+
+        /* build the server's Internet address */
+        bzero((char *) &serveraddr, sizeof(serveraddr));
+        serveraddr.sin_family = AF_INET;
+        serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        serveraddr.sin_port = htons(PORT);
+
+        /* bind: associate the parent socket with a port */
+        if (bind(sockfd, (struct sockaddr *) &serveraddr,
+	         sizeof(struct sockaddr)) < 0) {
+                perror("ERROR on binding");
+								exit(0);
+				}
+
+        /* listen: make this socket ready to accept connection requests */
+        if (listen(sockfd, BACKLOG) < 0) {
+                perror("ERROR on listen");
+								exit(0);
+				}
+
+	/* handler SIGCHLD signal */
+	signal(SIGCHLD, sigchld_handler);
+
+        /* wait for a connection request */
+	while(1) {  // main accept() loop
+    clientlen = sizeof(struct sockaddr_in);
+		new_fd = accept(sockfd, (struct sockaddr *)&clientaddr,&clientlen);
+
+		if (new_fd == -1) {
+			perror("ERROR on accept");
+			exit(0);
+		}
+
+		/* gethostbyaddr: determine who sent the message */
+                hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+			          sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+                if (hostp == NULL) {
+                        perror("ERROR on gethostbyaddr");
+												exit(0);
+								}
+                hostaddrp = inet_ntoa(clientaddr.sin_addr);
+                if (hostaddrp == NULL) {
+                        perror("ERROR on inet_ntoa\n");
+												exit(0);
+								}
+                //printf("server got connection from %s (%s)\n",
+		//       hostp->h_name, hostaddrp);
+		printf("Received request from Client: %s:%d\n", hostaddrp, PORT);
+
+		if (!fork()) { // this is the child process
+			close(sockfd); // child doesn't need the listener
+			timer = time(NULL);
+			if (send(new_fd, ctime(&timer), 30, 0) == -1){
+				perror("send");
+			}
+			close(new_fd);
+			exit(0);
+		}
+		close(new_fd);  // parent doesn't need this
+	}
+	return 0;
+}
+
+/*------------------------------------------------------------------------
+ * sigchld_handler - reaper of zombi child processes
+ *------------------------------------------------------------------------
+ */
+void sigchld_handler(int s)
+{
+	while(waitpid(-1, NULL, WNOHANG) > 0);
+}
---
+++ time.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <signal.h>
+#include <time.h>
+
+#define PORT 37
+
+int sockfd;
+
+void sigint_handler(int signo)
+{
+	/* things to do after a Ctrl-C, before finishing the program*/
+	puts("SIGINT received, closing program");
+	exit(0);
+}
+
+
+int main(int argc, char *argv[])
+{
+	struct sockaddr_in serveraddr;
+	char *h;
+  char *host;
+  char *p;
+  char *m;
+  char *mode;
+	int port,seconds,n;
+  struct hostent *server;
+
+        /* handler SIGINT signal*/
+	signal(SIGINT, sigint_handler);
+
+        /* check command line arguments */
+	switch (argc) {
+	case 7://Consulta TCP
+    h = argv[1];
+		host = argv[2];
+		p = argv[3];
+    port = atoi(argv[4]);
+    m = argv[5];
+    mode = argv[6];
+
+    if(strcmp (h,"-h") == 0 && strcmp (p,"-p") == 0 && strcmp (m,"-m") == 0 && strcmp (mode,"ct") == 0){
+      printf("TCP_Client\n");
+			while(1){
+      /* socket: create the socket */
+      sockfd = socket(AF_INET, SOCK_STREAM, 0);
+      if (sockfd < 0) {
+              perror("ERROR opening socket");
+              exit(0);
+      }
+
+      /* gethostbyname: get the server's DNS entry */
+      server = gethostbyname(host);
+      if (server == NULL) {
+              fprintf(stderr,"ERROR, no such host: %s\n", host);
+              exit(0);
+      }
+
+      /* build the server's Internet address */
+      bzero((char *) &serveraddr, sizeof(serveraddr));
+      serveraddr.sin_family = AF_INET;
+      bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+      serveraddr.sin_port = htons(port);
+
+      /* connect: create a connection with the server */
+      if (connect(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
+              perror("ERROR connecting");
+              exit(0);
+      }
+
+      char output[4096];
+
+      n = recv(sockfd, &seconds, sizeof(int), 0);
+      if (n < 0) {
+              perror("ERROR reading from socket");
+              exit(0);
+      }
+
+      time_t sec_today = ntohl(seconds) - 2208988800;
+
+      strftime(output, 4096, "%+", localtime(&sec_today));
+
+      printf("%s\n", output);
+		}
+
+      close(sockfd);
+
+    }else{
+
+      fprintf(stderr, "usage: %s <-h> <host> <-p> <port> <-m> <ct>\n", argv[0]);
+  		exit(1);
+
+    }
+		break;
+  case 3://Servidor
+    m = argv[1];
+    mode = argv[2];
+
+    if(strcmp (m,"-m") == 0 && strcmp (mode,"s") == 0){
+      //Server();
+      printf("Server\n");
+    }else{
+      fprintf(stderr, "usage: %s <-m> <s>\n", argv[0]);
+  		exit(1);
+    }
+    break;
+  case 5://Consulta UDP
+    h = argv[1];
+    host = argv[2];
+    m = argv[3];
+    mode = argv[4];
+
+    if(strcmp (h,"-h") == 0 && strcmp (m,"-m") == 0 && strcmp (mode,"cu") == 0){
+      //UDP_Client();
+      printf("UDP_Client\n");
+			/* socket: create the socket */
+			sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+			if (sockfd < 0) {
+							perror("ERROR opening socket");
+							exit(0);
+			}
+
+			/* gethostbyname: get the server's DNS entry */
+			server = gethostbyname(host);
+			if (server == NULL) {
+							fprintf(stderr,"ERROR, no such host: %s\n", host);
+							exit(0);
+			}
+
+			/* build the server's Internet address */
+			bzero((char *) &serveraddr, sizeof(serveraddr));
+			serveraddr.sin_family = AF_INET;
+			bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+			serveraddr.sin_port = htons(PORT);
+
+			/* connect: create a connection with the server */
+      if (connect(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
+              perror("ERROR connecting");
+              exit(0);
+      }
+
+				//UDP mandar datagrama vacio
+			n = send(sockfd, NULL, 0, 0);
+			if (n < 0) {
+							perror("ERROR writing to socket");
+							exit(0);
+			}
+			char output[4096];
+
+			n = recv(sockfd, &seconds, sizeof(int), 0);
+			if (n < 0) {
+							perror("ERROR reading from socket");
+							exit(0);
+			}
+
+			time_t sec_today = ntohl(seconds) - 2208988800;
+
+			strftime(output, 4096, "%+", localtime(&sec_today));
+
+			printf("%s\n", output);
+
+    }else{
+      fprintf(stderr, "usage: %s <-h> <host> <-m> <cu>\n", argv[0]);
+  		exit(1);
+    }
+    break;
+	default:
+		fprintf(stderr, "usage: %s <-h> <host> <-p> <port> <-m> <mode> <-d>\n", argv[0]);
+		exit(1);
+  }
+	return(0);
+}
